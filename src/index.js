de3c95f12d12aaac43070a2caefb2c8c590bb47f
kfrn <knfrances@gmail.com>
2019-02-01 22:33:15 +1300

    Styling: install Bulma 0.7.2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import './main.css';
-import { Elm } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
+import "./styles/bulma.min.css";
+import "./styles/main.css";
+import { Elm } from "./Main.elm";
+import registerServiceWorker from "./registerServiceWorker";
 
 Elm.Main.init({
-  node: document.getElementById('root')
+  node: document.getElementById("root")
 });
 
 registerServiceWorker();
