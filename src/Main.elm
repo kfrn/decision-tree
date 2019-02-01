918a9703cf95807b845e79b1e6bbd5c13a123b51
kfrn <knfrances@gmail.com>
2019-02-01 19:05:38 +1300

    Initial commit ☽
    Created with create-elm-app, then the Elm app broken out into different modules.

---
+++ src/Main.elm
@@ -0,0 +1,17 @@
+module Main exposing (main)
+
+import Browser
+import Messages exposing (Msg)
+import Model exposing (Model, init)
+import Update exposing (update)
+import View exposing (view)
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
